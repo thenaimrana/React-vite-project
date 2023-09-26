551a6f10f450750dacdc690593cc67f9144a205c
Md Naim Rana <mdnaimrana5@gmail.com>
2023-09-27 00:37:39 +0600

    jsx ternary and iif

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,41 @@ import Hero from "./assets/component/Hero";
 
 
 const App = () => {
+
+  const number = 50;
+  const mark = 80;
+  const marks = 80;
   return (
     <div>
       <Header/>
       <Hero/>
       <ContactForm/>
       <Footer/>
+
+      <button onClick={()=>alert("hello")}>Submit</button>
+
+      <h1 style={{color:"red"}}>Hello world</h1>
+
+      {number==50?<h1>Yor are Right</h1>:<h1>Your are Rong</h1>}
+      {number!==50?<h1>Yor are Right</h1>:<h1>Your are Rong</h1>}
+
+
+
+
+
+
+      {(()=>{
+
+
+        if(mark>=80 && mark<100){
+          return <h1>A+</h1>
+        }
+
+        })()}
+
+
+
+
     </div>
   );
 };
--- src/assets/component/Header.jsx
+++ src/assets/component/Header.jsx
@@ -2,9 +2,22 @@
 const Header = () => {
     return (
         <div>
-            <ul>
-                <li>Home</li>
-                <li>Others</li>
+            <ul style={
+                {listStyle:"none",
+                 display:"flex",
+                  margin:"20px",
+                  background:"pink",
+                  padding:"20px",
+                }
+                }>
+                <li style={{marginLeft:"25px"}}>Home</li>
+                <li style={{marginLeft:"25px"}}>Home</li>
+                <li style={{marginLeft:"25px"}}>Home</li>
+                <li style={{marginLeft:"25px"}}>About</li>
+                <li style={{marginLeft:"25px"}}>Blogs</li>
+                <li style={{marginLeft:"25px"}}>Servics</li>
+                <li style={{marginLeft:"25px"}}>Page</li>
+                <li style={{marginLeft:"25px"}}>Others</li>
             </ul>
         </div>
     );
